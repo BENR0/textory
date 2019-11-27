Fix tpi boundaries wrong results

textory/util.py
@@ -343,7 +343,7 @@ def convolution(x, win_size=5, win_geom="square", kernel=None, **kwargs):
     
     if isinstance(x, da.core.Array):
         conv_padding = int(win_size//2)
-        res = x.map_overlap(pcon, depth={0: conv_padding, 1: conv_padding})
+        res = x.map_overlap(pcon, depth={0: conv_padding, 1: conv_padding}, boundary={0:np.nan, 1:np.nan})
     else:
         res = pcon(x)
     